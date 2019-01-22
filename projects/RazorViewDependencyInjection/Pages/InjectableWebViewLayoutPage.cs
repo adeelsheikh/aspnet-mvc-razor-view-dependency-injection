07ed611b356b5c747593659882f8b9b17b6ccb1b
Adeel Nayyer Sheikh <adeelnayyersheikh@gmail.com>
2019-01-22 08:45:25 +0400

    Dependency injection into Layout page.

---
+++ projects/RazorViewDependencyInjection/Pages/InjectableWebViewLayoutPage.cs
@@ -0,0 +1,17 @@
+﻿using RazorViewDependencyInjection.Contracts;
+using System.Web.Mvc;
+
+namespace RazorViewDependencyInjection.Pages
+{
+    public abstract class InjectableWebViewLayoutPage<T> : WebViewPage<T>
+    {
+        public ITokenService TokenService { get; set; }
+
+        public override void InitHelpers()
+        {
+            TokenService = DependencyResolver.Current.GetService<ITokenService>();
+
+            base.InitHelpers();
+        }
+    }
+}
